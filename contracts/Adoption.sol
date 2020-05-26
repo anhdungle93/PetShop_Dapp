e18fe5a977cdf750cc511d05de3b425429a0ebfb
anhdungle <anhdungle634@gmail.com>
2020-05-26 10:42:04 +0200

    add getAdopters function

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -3,12 +3,18 @@ pragma solidity ^0.5.0;
 contract Adoption {
   address[16] public adopters;
 
+  // Adopting a pet
   function adopt(uint petId) public returns (uint) {
     require(petId >= 0 && petId <= 15, 'invalid petId');
 
     adopters[petId] = msg.sender;
     
     return petId;
+  }
+
+  // Retrieving the adopters
+  function getAdopters() public view returns (address[16] memory) {
+    return adopters;
   }
   
 }
